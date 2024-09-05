23835f51fa458f1a1e3d3f73fad90aba9cd76d2f
Alexandre Angrignon <alex040892@gmail.com>
2024-09-05 17:21:12 +1200

    Features Section

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,17 @@
 'use client';
 
 import Hero from '@/components/Hero';
-import Image from 'next/image';
 import { ReactLenis } from 'lenis/dist/lenis-react';
 import Brands from '@/components/Brands';
+import Features from '@/components/Features';
+
 export default function Home() {
   return (
     <>
       <ReactLenis root>
         <Hero />
         <Brands />
+        <Features />
         {/* temporary div */}
         {/* <div className='h-[8000px]'></div> */}
       </ReactLenis>
