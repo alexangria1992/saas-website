2211c3f4a89725df46d97a58c669211b5aa2a01e
Alexandre Angrignon <alex040892@gmail.com>
2024-09-05 15:07:24 +1200

    Brands Secttion

--- app/page.jsx
+++ app/page.jsx
@@ -3,13 +3,15 @@
 import Hero from '@/components/Hero';
 import Image from 'next/image';
 import { ReactLenis } from 'lenis/dist/lenis-react';
+import Brands from '@/components/Brands';
 export default function Home() {
   return (
     <>
       <ReactLenis root>
         <Hero />
+        <Brands />
         {/* temporary div */}
-        <div className='h-[8000px]'></div>
+        {/* <div className='h-[8000px]'></div> */}
       </ReactLenis>
     </>
   );
---
+++ motion/motionVariants.jsx
@@ -0,0 +1,26 @@
+export const fadeInOnScroll = (delay, duration) => ({
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1, //fade in to fully visible
+    transition: {
+      delay: delay, // small delay before starting the animation
+      duration: duration, // duration of the dade-in effect
+      ease: [0.42, 0, 0.58, 1], // custom cubic-bezier
+    },
+  },
+});
+
+export const fadeInUpSpring = (delay, duration) => ({
+  hidden: { opacity: 0, y: 60, scale: 0.8 },
+  visible: {
+    opacity: 1, //fade in to fully visible
+    scale: 1,
+    transition: {
+      delay: delay,
+      duration: duration,
+      ease: [0.6, -0.05, 0.01, 0.99],
+      type: 'spring',
+      stiffness: 100,
+    },
+  },
+});
